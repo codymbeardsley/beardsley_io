9c2bcf1faad8c19ebd15f8a54d4d26201a632dfe
Cody Beardsley <beardsley.cody@gmail.com>
2015-09-01 23:45:42 -0700

    Twitter api modification

--- index.js
+++ index.js
@@ -3,30 +3,27 @@ var cors = require('cors');
 var app = express();
 
 var corsOptions = {
-  origin: 'http://run.plunkr.com'
+  origin: 'http://run.plnkr.co'
 };
-
-var twitterAPI = require('node-twitter-api');
-var twitter = new twitterAPI({
-    	consumerKey: '8qU3Vr6qYk7LHZ7z6dAKL4q1I',
-    	consumerSecret: '6CrR6RlXOpwNcpDqPExFEC9ep0h4XfYHiZyem0NAEox7nWisQN',
-    	callback: ''
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+ 	consumer_key: '8qU3Vr6qYk7LHZ7z6dAKL4q1I',
+ 	consumer_secret: '6CrR6RlXOpwNcpDqPExFEC9ep0h4XfYHiZyem0NAEox7nWisQN',
+ 	access_token_key: '3422127673-cnt1o8h93VXtjPfkRw7tAAnjFuCJcskX99I6QPy',
+ 	access_token_secret: 'yQUxfwweesBdDUtQnUJLieY5POLhvVcRmsfjZ3dAxYLH0'
 });
-var requestToken;
-var requestTokenSecret;
-twitter.getRequestToken(function(error, _requestToken, _requestTokenSecret, results){
-	   	if (error) {
-	       	console.log(error);
-	    	} else {
-	    		requestToken = _requestToken;
-	    		requestTokenSecret = _requestTokenSecret;
-	    	}
-	});
+ 
 
 app.use(express.static(__dirname + '/static'));
 
 app.get('/twittertoken', cors(corsOptions), function (req, res) {
-	res.json({requestToken: requestToken, requestTokenSecret: requestTokenSecret});
+	var params = {screen_name: 'angularjs'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+		if (!error) {
+			res.json({tweets: tweets});
+		}
+	});
 });
 
 app.listen(3000);
